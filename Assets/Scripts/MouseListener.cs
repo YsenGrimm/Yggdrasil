ca4dda322f5a4e662f6666778c3b9b9b265504e9
YsenGrimm <kloeten.wars@googlemail.com>
2014-08-23 14:57:44 +0200

    Added mouse listening to move screen

---
+++ Assets/Scripts/MouseListener.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseListener : MonoBehaviour {
+
+	public float MoveSpeed = 3.0f;
+	public float xOffest = 50;
+	public float yOffset = 50;
+
+	Rect MouseDeadZone;
+	int width = Screen.width;
+	int height = Screen.height;
+	Vector3 mousePosition = new Vector3();
+	GameObject ctrlCamera;
+
+	// Use this for initialization
+	void Start () {
+		MouseDeadZone = new Rect(xOffest, yOffset, width - (xOffest*2), height - (yOffset*2));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mousePosition = Input.mousePosition;
+		Vector3 cameraPosition = Camera.main.transform.position;
+		if (mousePosition.x > (MouseDeadZone.x + MouseDeadZone.width)) {
+			Camera.main.transform.position = new Vector3(cameraPosition.x + MoveSpeed, cameraPosition.y, cameraPosition.z);
+		} else if (mousePosition.x < MouseDeadZone.x) {
+			Camera.main.transform.position = new Vector3(cameraPosition.x - MoveSpeed, cameraPosition.y, cameraPosition.z);
+		} else if (mousePosition.y < MouseDeadZone.y) {
+			Camera.main.transform.position = new Vector3(cameraPosition.x, cameraPosition.y - MoveSpeed, cameraPosition.z);
+		} else if (mousePosition.y > (MouseDeadZone.y + MouseDeadZone.height)){
+			Camera.main.transform.position = new Vector3(cameraPosition.x, cameraPosition.y + MoveSpeed, cameraPosition.z);
+		}
+	}
+}
