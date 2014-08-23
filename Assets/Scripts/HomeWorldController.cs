8151fa4891546df8d3acb77ec5789d164205e44e
YsenGrimm <kloeten.wars@googlemail.com>
2014-08-23 15:40:51 +0200

    Basic world spawning, and (really) basic home world interaction added

---
+++ Assets/Scripts/HomeWorldController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomeWorldController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown() {
+		//TODO: Switch to world interaction mode
+		Debug.Log("Interacted with HomeWorld");
+	}
+}
---
+++ Assets/Scripts/WorldSpawner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WorldSpawner : MonoBehaviour {
+
+	public GameObject World;
+	public float DensityRadius = 6.0f;
+	public int Count = 10;
+	public Rect WorldSize = new Rect(-100, -100, 200, 200);
+
+	List<GameObject> worldList = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < Count; ++i) {
+
+			int randX = Random.Range((int)WorldSize.x, (int)(WorldSize.x + WorldSize.width));
+			int randY = Random.Range((int)WorldSize.y, (int)(WorldSize.y + WorldSize.height));
+
+			GameObject instWorld = GameObject.Instantiate(World) as GameObject;
+			instWorld.transform.position = new Vector3(randX, randY, 0);
+
+			// TODO: if space gets to messy, check here
+			/*
+			foreach (GameObject w in worldList) {
+				Vector2 wPos = new Vector2(w.transform.position.x, w.transform.position.y);
+				Vector2 instPos = new Vector2(randX, randY);
+
+				if ((wPos - instPos).magnitude < DensityRadius) {
+					break;
+				} else {
+
+					worldList.Add(instWorld);
+				}
+			}
+			*/
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
